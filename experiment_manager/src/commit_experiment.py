1f4d05eb8d6b0873544af676cdc9c8286a222a73
Vasiliy Kuzmin <vasilykuzmin22@gmail.com>
2024-04-26 22:37:39 +0300

    Added mongo to ci/cd

--- experiment_manager/src/commit_experiment.py
+++ experiment_manager/src/commit_experiment.py
@@ -11,38 +11,31 @@ from secrets import token_hex
 def getMainDir():
     return pathlib.Path(__file__).parent.parent.parent.absolute()
 
-def gitRecoveryWrapper(func):
-    '''Recovers git HEAD and index state.'''
-    def wrap(*args, **kwargs):
-        dirPath = getMainDir()
-        os.makedirs(f'{dirPath}/tmp', exist_ok=True)
-        shutil.copy(f'{dirPath}/.git/index', f'{dirPath}/tmp/index')
-        returns = func(*args, **kwargs)
-        shutil.move(f'{dirPath}/tmp/index', f'{dirPath}/.git/index')
-        return returns
-    return wrap
-
 
 def getRepo() -> git.Repo:
     dirPath = getMainDir()
     return git.Repo(dirPath)
 
 
-@gitRecoveryWrapper
 def commit(file_to_execute) -> str:
     '''Commits all changes to an experiment's commit.'''
+    experiment_name = f'experiment-{token_hex(16)}'
+
     repo = getRepo()
     original_branch = repo.active_branch
-    experiment_branch = repo.create_head(f'experiment-{token_hex(16)}')
 
+    repo.git.stash(experiment_name)
+
+    experiment_branch = repo.create_head(experiment_name)
     repo.git.checkout(experiment_branch)
 
+    repo.git.stash.apply(experiment_name)
     repo.git.add(update=True)
     repo.index.commit(str(file_to_execute))
     repo.git.push('gitlab', experiment_branch)
 
     repo.git.checkout(original_branch)
-
+    repo.git.stash.pop(experiment_name)
 
 if __name__ == '__main__':
     commit(sys.argv[1])
